c6ce769ffa21c1ee60f3ea3609da63d62146b239
Gabriel Inzirillo <gabriel.inzirillo@ajar.ch>
2016-06-14 16:23:46 +0200

    Add a worked time attribute in the work time data class and display it on the front-end. Add a filter in the angular application to format a millisecond to an hours readable format.

--- workHours/database/methods/WorkTime/WorkTime-events.js
+++ workHours/database/methods/WorkTime/WorkTime-events.js
@@ -3,3 +3,21 @@
 model.WorkTime.events.save = function(event) {
 	this.userID = currentSession().user.ID;
 };
+
+
+model.WorkTime.timeWorked.onGet = function() {
+	var returnValue = 0,
+		start = this.start,
+		end = this.end;
+		breakTime = this['break'];
+	
+	if(start != null && end != null){
+		if(breakTime != null){
+			returnValue = (end-start)-breakTime;
+		}else{
+			returnValue = (end-start);
+		}
+	}
+	
+	return returnValue;
+};

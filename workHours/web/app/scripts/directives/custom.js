afbf96505aaf178c34abb2a49238ef72fe439fa4
Gabriel Inzirillo <gabriel.inzirillo@ajar.ch>
2016-06-27 12:51:26 +0200

    Don't use date picker if we use chrome (the date input type is working nicely on chrome)

--- workHours/web/app/scripts/directives/custom.js
+++ workHours/web/app/scripts/directives/custom.js
@@ -27,19 +27,22 @@
 			restrict: 'A',
 			require: 'ngModel',
 			link: function(scope, element, attrs, ngModelCtrl) {
-				var dateToSet = attrs.datepicker != '' && /\d{4}[-](0?[1-9]|1[012])[-](0[1-9]|[1-2][0-9]|3[01])/.test(attrs.datepicker) ? attrs.datepicker : new Date(); // Set the date of today if we don't get a date in the appropriate format (yyyy-mm-dd)
-				$(element).datepicker({
-					dateFormat: 'yy-mm-dd',
-					onSelect: function(date) {
-						scope.$apply(function() {
-							//debugger;
-							ngModelCtrl.$setViewValue(date);
-						});
-					}
-				});
-				setTimeout(function(){
-					$(element).datepicker('setDate',dateToSet);
-				},100);
+				var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+				if(!isChrome){
+					var dateToSet = attrs.datepicker != '' && /\d{4}[-](0?[1-9]|1[012])[-](0[1-9]|[1-2][0-9]|3[01])/.test(attrs.datepicker) ? attrs.datepicker : new Date(); // Set the date of today if we don't get a date in the appropriate format (yyyy-mm-dd)
+					$(element).datepicker({
+						dateFormat: 'yy-mm-dd',
+						onSelect: function(date) {
+							scope.$apply(function() {
+								//debugger;
+								ngModelCtrl.$setViewValue(date);
+							});
+						}
+					});
+					setTimeout(function(){
+						$(element).datepicker('setDate',dateToSet);
+					},100);
+				}
 			}
 		}
 	});

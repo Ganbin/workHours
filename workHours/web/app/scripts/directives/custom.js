dc8365674aa0749bcdacbe1221a9848777d3eba5
Gabriel Inzirillo <gabriel.inzirillo@ajar.ch>
2016-06-27 12:39:27 +0200

    Add a custom directive that create a date picker and set the current Date to the date given in params (format : yyyy-mm-dd) or the current date if the format is incorrect or no value

--- workHours/web/app/scripts/directives/custom.js
+++ workHours/web/app/scripts/directives/custom.js
@@ -1,26 +1,47 @@
-(function(angular,moment,utils){
+(function(angular, moment, utils) {
 	'use strict';
-	
+
 	angular.module('workTime')
-	
+
 	/* Attach to input time elements and switch their formatting to be HH:MM
-	 */             
-	.directive('ngModel', function( $filter ) {
-	    return {
-	        require: '?ngModel',
-	        link: function(scope, elem, attr, ngModel) {
-	            if( !ngModel ){
-	                return;
-	            }
-	            if( attr.type !== 'time' ){
-	                return;
-	            }
-	                    
-	            ngModel.$formatters.unshift(function(value) {
-	                return value.replace(/:\d{2}[.,]\d{3}$/, '');//
-	            });
-	        }
-	    };
+	 */
+	.directive('ngModel', function($filter) {
+		return {
+			require: '?ngModel',
+			link: function(scope, elem, attr, ngModel) {
+				if(!ngModel) {
+					return;
+				}
+				if(attr.type !== 'time') {
+					return;
+				}
+
+				ngModel.$formatters.unshift(function(value) {
+					return value.replace(/:\d{2}[.,]\d{3}$/, ''); //
+				});
+			}
+		};
+	})
+	.directive('datepicker', function() {
+		return {
+			restrict: 'A',
+			require: 'ngModel',
+			link: function(scope, element, attrs, ngModelCtrl) {
+				var dateToSet = attrs.datepicker != '' && /\d{4}[-](0?[1-9]|1[012])[-](0[1-9]|[1-2][0-9]|3[01])/.test(attrs.datepicker) ? attrs.datepicker : new Date(); // Set the date of today if we don't get a date in the appropriate format (yyyy-mm-dd)
+				$(element).datepicker({
+					dateFormat: 'yy-mm-dd',
+					onSelect: function(date) {
+						scope.$apply(function() {
+							//debugger;
+							ngModelCtrl.$setViewValue(date);
+						});
+					}
+				});
+				setTimeout(function(){
+					$(element).datepicker('setDate',dateToSet);
+				},100);
+			}
+		}
 	});
-	
-}(window.angular,window.moment,window.utils));
+
+}(window.angular, window.moment, window.utils));

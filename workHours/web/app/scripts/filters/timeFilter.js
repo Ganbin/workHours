b2c96c4e36943d27eb3645b9788c0bcee94e3d74
Gabriel Inzirillo <gabriel.inzirillo@ajar.ch>
2016-07-08 11:53:38 +0200

    Fix a bug when when filter the time. floor is more appropriate than round because round will put 6.5 to 7, when we want to keep only the 6.

--- workHours/web/app/scripts/filters/timeFilter.js
+++ workHours/web/app/scripts/filters/timeFilter.js
@@ -4,7 +4,7 @@
     // Add minutesToHours filter, this filter will show the time in hours (hh:mm) from a value in minutes
     angular.module('workTime').filter('minutesToHours', function() {
 		return function (minutes){
-			var date = (((Math.round(minutes/60)) < 10) ? ("0" + (Math.round(minutes/60))) : (Math.round(minutes/60))) + ':' + (((minutes%60) < 10) ? ("0" + (minutes%60)) : (minutes%60));
+			var date = (((Math.floor(minutes/60)) < 10) ? ("0" + (Math.floor(minutes/60))) : (Math.floor(minutes/60))) + ':' + (((minutes%60) < 10) ? ("0" + (minutes%60)) : (minutes%60));
 			return date;
 		};
 	});
@@ -13,7 +13,7 @@
     angular.module('workTime').filter('millisToHours', function() {
 		return function (millis){
 			var date, minutes = millis/1000/60;
-			date = (((Math.round(millis/60)) < 10) ? ("0" + (Math.round(minutes/60))) : (Math.round(minutes/60))) + ':' + (((minutes%60) < 10) ? ("0" + (minutes%60)) : (minutes%60));
+			date = (((Math.floor(millis/60)) < 10) ? ("0" + (Math.floor(minutes/60))) : (Math.floor(minutes/60))) + ':' + (((minutes%60) < 10) ? ("0" + (minutes%60)) : (minutes%60));
 			return date;
 		};
 	});

792115b186be60ae1df42703083fdf5086de6e7e
Gabriel Inzirillo <gabriel.inzirillo@ajar.ch>
2016-11-30 18:07:50 +0100

    (model) remove the user time data class and add a restrict event on the work time class to restrict the time only for the user, or all for dataAdmin. change the permissions. some debug of the event of work time dataclass

--- workHours/database/methods/WorkTime/WorkTime-events.js
+++ workHours/database/methods/WorkTime/WorkTime-events.js
@@ -1,19 +1,16 @@
-
-
 model.WorkTime.events.save = function(event) {
 	this.userID = currentSession().user.ID;
 	this.modificationDate = new Date();
 };
 
-
 model.WorkTime.timeWorked.onGet = function() {
 	var returnValue = 0,
 		start = this.start,
 		end = this.end;
-		breakTime = this['break'];
+		breakTime = this.breakTimeMs;
 		
 	if(start != null && end != null){
-		if(breakTime != null){
+		if(breakTime != null || breakTime != 0){
 			returnValue = (end-start)-breakTime;
 		}else{
 			returnValue = (end-start);
@@ -23,29 +20,25 @@ model.WorkTime.timeWorked.onGet = function() {
 	return returnValue;
 };
 
-
 model.WorkTime.userName.onGet = function() {
 	var user = directory.internalStore.User.find('ID == :1',this.userID);
 	return user.fullName || '';
 };
-
 
 model.WorkTime.events.init = function(event) {
 	this.creationDate = new Date();
 	this.modificationDate = new Date();
 };
 
-
 model.WorkTime.events.validate = function(event) {
-	if((this['break'] != null && this.breakReason == null) || (this.breakReason != null && this['break'] == null)){
+	if(this.isNew() && (((this.breakTime != null) && this.breakReason == null) || (this.breakReason != null && (this.breakTime == null)))){
 		return {error: 10, errorMessage: 'You must have a reason for a break time!'};
 	}
 	
-	if((this.trainTime != null && this.trainPrice == null) || (this.trainPrice != null && this.trainTime == null)){
+	if(this.isNew() && (this.trainTime != null && this.trainPrice == null) || (this.trainPrice != null && this.trainTime == null)){
 		return {error: 11, errorMessage: 'You must have a price with a train time!'};
 	}
 };
-
 
 model.WorkTime.breakTime.events.validate = function(event) {
 	if(this.breakTime != null && !this.breakTime.match(/([01]{1}[0-9]|2[0-3]):[0-5][0-9]/)){
@@ -59,11 +52,31 @@ model.WorkTime.trainTime.events.validate = function(event) {
 	}
 };
 
-
 model.WorkTime.breakTimeMs.onGet = function() {
-	return ((parseInt(this.breakTime.substr(0,2))*60)+parseInt(this.breakTime.substr(3,2)))*60*1000;
+	if (this.breakTime != null) {
+		return ((parseInt(this.breakTime.substr(0,2))*60)+parseInt(this.breakTime.substr(3,2)))*60*1000;
+	}
+	
+	return 0;
 };
 
 model.WorkTime.trainTimeMs.onGet = function() {
-	return ((parseInt(this.trainTime.substr(0,2))*60)+parseInt(this.trainTime.substr(3,2)))*60*1000;
+	if (this.trainTime != null) {
+		return ((parseInt(this.trainTime.substr(0,2))*60)+parseInt(this.trainTime.substr(3,2)))*60*1000;
+	}
+	
+	return 0;
+};
+
+
+model.WorkTime.events.restrict = function(event) {
+	var returnCol = ds.WorkTime.createEntityCollection();
+	
+	if (currentSession().belongsTo('DataAdmin')) {
+		returnCol = ds.WorkTime.all();
+	} else {
+		returnCol = ds.WorkTime.query('userID == :$userid');
+	}
+	
+	return returnCol;
 };

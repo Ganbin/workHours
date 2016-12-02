267ad3340698568524d1878566e55854e73a6b82
Gabriel Inzirillo <gabriel.inzirillo@ajar.ch>
2016-12-02 10:08:57 +0100

    (addition) Add a tab view in the user view to let the dataAdmin to modify users and bind them with the directoryUser

--- workHours/web/prototype/index.waPage/scripts/index.js
+++ workHours/web/prototype/index.waPage/scripts/index.js
@@ -2,16 +2,45 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var menuItem2 = {};	// @menuItem
+	var documentEvent = {};	// @document
+	var button1 = {};	// @Button
 	var login1 = {};	// @login
 	var setCategoryClientBtn = {};	// @Button
 // @endregion// @endlock
 
+function resetlinkDirectoryDisplayTxt() {
+	setTimeout(function () {$$('linkDirectoryDisplayTxt').value("");}, 5000)	
+;}
+
 // eventHandlers// @lock
+
+	menuItem2.click = function menuItem2_click (event)// @startlock
+	{// @endlock
+		dirUser = directoryComponent.getUsers();
+		waf.sources.dirUser.sync();
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		$$('component1').loadComponent();
+	};// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		waf.sources.user.userID = waf.sources.dirUser.ID;
+		waf.sources.user.save({onSuccess:function(evt){
+			$$('linkDirectoryDisplayTxt').value("Done");
+			resetlinkDirectoryDisplayTxt();
+		}, onError:function(err) {
+			$$('linkDirectoryDisplayTxt').value("Error");
+			resetlinkDirectoryDisplayTxt();
+		}});
+	};// @lock
 
 	login1.login = function login1_login (event)// @startlock
 	{// @endlock
-		debugger;
-		waf.sources.category.resolveSource();
+		
 	};// @lock
 
 	setCategoryClientBtn.click = function setCategoryClientBtn_click (event)// @startlock
@@ -24,6 +53,9 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("menuItem2", "click", menuItem2.click, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+	WAF.addListener("button1", "click", button1.click, "WAF");
 	WAF.addListener("login1", "login", login1.login, "WAF");
 	WAF.addListener("setCategoryClientBtn", "click", setCategoryClientBtn.click, "WAF");
 // @endregion

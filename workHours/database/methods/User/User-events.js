ea8671c3a6d02a1073c60a515605bbd89cf3eb25
Gabriel Inzirillo <gabriel.inzirillo@ajar.ch>
2016-12-05 19:59:47 +0100

    (addition) add restrict event on the User data class
    Only the dataAdmin can see all users, a normal user can only see himself

--- workHours/database/methods/User/User-events.js
+++ workHours/database/methods/User/User-events.js
@@ -3,3 +3,16 @@
 model.User.fullName.onGet = function() {
 	return this.firstName+" "+this.lastName;
 };
+
+
+model.User.events.restrict = function(event) {
+	var returnCol = ds.User.createEntityCollection();
+	
+	if (currentSession().belongsTo('DataAdmin')) {
+		returnCol = ds.User.all();
+	} else {
+		returnCol = ds.User.query('userID == :$userid');
+	}
+	
+	return returnCol;
+};

1aa1622d4c54c81d904dde771d0b7aab5072b7be
Gabriel Inzirillo <gabriel.inzirillo@ajar.ch>
2016-12-06 13:39:55 +0100

    (addition) add currency value converter

--- workHours/web/aurelia/src/resources/index.js
+++ workHours/web/aurelia/src/resources/index.js
@@ -3,6 +3,7 @@ export function configure(config) {
                             './value-converters/millisToHours',
                             './value-converters/minutesToHours',
                             './value-converters/dateFormat',
-                            './value-converters/truncate'
+                            './value-converters/truncate',
+                            './value-converters/currency'
                         ]);
 }
